a8ed93f71797a6ae9b9176dbd92d113c0adca973
arai1999 <arai1999@hotmail.com>
2024-03-28 20:25:12 -0700

    Maven setup to export to devworld

--- src/main/java/me/dollarmc/minecraftinsanedifficulty/MinecraftInsaneDifficulty.java
+++ src/main/java/me/dollarmc/minecraftinsanedifficulty/MinecraftInsaneDifficulty.java
@@ -7,11 +7,12 @@ public final class MinecraftInsaneDifficulty extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Minecraft Insane Difficulty has started running");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Minecraft Insane Difficulty has stopped running");
     }
 }
